3fec4382e80855a1d432deeaee611d7b9c872877
Paul Johnston <paul.johnston@portswigger.net>
2018-06-26 14:34:09 +0100

    Fix bug related to default ports

--- src/copy-sitemap.kt
+++ src/copy-sitemap.kt
@@ -3,11 +3,10 @@ package burp
 import java.awt.Component
 import java.awt.Frame
 import java.awt.event.ActionEvent
-import java.io.PrintStream
-import java.net.MalformedURLException
 import java.net.URL
 import java.util.*
 import javax.swing.*
+import kotlin.concurrent.thread
 
 
 class BurpExtender : IBurpExtender {
@@ -50,55 +49,54 @@ class ContextMenuListener(var invocation: IContextMenuInvocation) : AbstractActi
             source = URL(source.protocol, source.host, -1, source.file)
         }
         val target = JOptionPane.showInputDialog(e.source as Component, "Target URL", source.toString())
-        try {
-            val progressDialog = ProgressDialog(getBurpFrame(), true)
-            progressDialog.setLocationRelativeTo(getBurpFrame())
-            SwingUtilities.invokeLater {
-                progressDialog.isVisible = true
-            }
-            UrlCopier(progressDialog, source, URL(target)).start()
-        } catch (ex: MalformedURLException) {
-            BurpExtender.cb.printError(ex.toString())
+        val progressDialog = ProgressDialog(getBurpFrame(), true)
+        progressDialog.setLocationRelativeTo(getBurpFrame())
+        SwingUtilities.invokeLater {
+            progressDialog.isVisible = true
         }
+        UrlCopier(progressDialog, source, URL(target)).start()
     }
 }
 
 
-class UrlCopier(val progressDialog: ProgressDialog, val source: URL, val target: URL) : Thread() {
+class UrlCopier(val progressDialog: ProgressDialog, val source: URL, val target: URL) : Thread("UrlCopier") {
     fun hostHeader(url: URL): String {
         return if (url.port == -1 || url.port == url.defaultPort) { url.host } else { "${url.host}:${url.port}" }
     }
 
     override fun run() {
         val targetHost = hostHeader(target)
-        val targetService = BurpExtender.cb.helpers.buildHttpService(target.host, target.port, target.protocol)
+        val port = if(target.port == - 1) { target.defaultPort } else { target.port }
+        val targetService = BurpExtender.cb.helpers.buildHttpService(target.host, port, target.protocol)
 
         for (item in BurpExtender.cb.getSiteMap(source.toString())) {
             try {
-            if (item.response == null) {
-                continue
-            }
-            val requestInfo = BurpExtender.cb.helpers.analyzeRequest(item.httpService, item.request)
-            val headers = requestInfo.headers.toMutableList()
-            val body = Arrays.copyOfRange(item.request, requestInfo.bodyOffset, item.request.size)
-
-            headers[0] = headers[0].replace(source.path, target.path)
-            for (i in headers.indices) {
-                if (headers[i].startsWith("Host:")) {
-                    headers[i] = "Host: ${targetHost}"
-                    break
+                if (item.response == null) {
+                    continue
                 }
-            }
+                val requestInfo = BurpExtender.cb.helpers.analyzeRequest(item.httpService, item.request)
+                val headers = requestInfo.headers.toMutableList()
+                val body = Arrays.copyOfRange(item.request, requestInfo.bodyOffset, item.request.size)
 
-            val newItem = EditableHttpRequestResponse(item)
-            newItem.request = BurpExtender.cb.helpers.buildHttpMessage(headers, body)
-            newItem.httpService = targetService
-            BurpExtender.cb.addToSiteMap(newItem)
+                headers[0] = headers[0].replace(source.path, target.path)
+                for (i in headers.indices) {
+                    if (headers[i].startsWith("Host:")) {
+                        headers[i] = "Host: ${targetHost}"
+                        break
+                    }
+                }
+
+                val newItem = EditableHttpRequestResponse(item)
+                newItem.request = BurpExtender.cb.helpers.buildHttpMessage(headers, body)
+                newItem.httpService = targetService
+                BurpExtender.cb.addToSiteMap(newItem)
             } catch (ex: Exception) {
                 BurpExtender.cb.printError(ex.toString())
             }
         }
-        progressDialog.isVisible = false
+        SwingUtilities.invokeLater {
+            progressDialog.isVisible = false
+        }
     }
 }
 
